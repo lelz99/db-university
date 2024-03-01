08dbb16c02677bcd778bef10849a1194c3255e1f
lelz99 <emanuelesimonetti99@gmail.com>
2024-03-01 14:37:55 +0100

    es 5 select

--- query-select.sql
+++ query-select.sql
@@ -14,8 +14,10 @@ SELECT * FROM `students` WHERE `date_of_birth` <= '1994-03-01' ORDER BY `date_of
 laurea (286)
 SELECT * FROM `courses` WHERE `period` = 'I semestre' AND `year` = 1;
 
+
 5. Selezionare tutti gli appelli d esame che avvengono nel pomeriggio (dopo le 14) del
 20/06/2020 (21)
+SELECT * FROM `exams` WHERE `hour` >= '14%' AND `date` = '2020-06-20';
 
 
 6. Selezionare tutti i corsi di laurea magistrale (38)
