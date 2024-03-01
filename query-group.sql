aa9184f86856ac6e7cc4a19417fab1fc9c7b8aa1
lelz99 <emanuelesimonetti99@gmail.com>
2024-03-01 15:21:27 +0100

    es 3 group

--- query-group.sql
+++ query-group.sql
@@ -6,8 +6,8 @@ SELECT COUNT(*), YEAR(`enrolment_date`) FROM `students` GROUP BY YEAR(`enrolment
 SELECT COUNT(*), `office_address` FROM `teachers` GROUP BY `office_address`;
 
 
-
 -- 3. Calcolare la media dei voti di ogni appello d esame
+SELECT ROUND(AVG(`vote`), 2), `exam_id` FROM `exam_student` GROUP BY `exam_id`;
 
 
 -- 4. Contare quanti corsi di laurea ci sono per ogni dipartimento
