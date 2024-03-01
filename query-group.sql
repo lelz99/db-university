d160027cb0b3606f5b3a2ac0ab9484f1b1120692
lelz99 <emanuelesimonetti99@gmail.com>
2024-03-01 15:05:15 +0100

    es 2 group

--- query-group.sql
+++ query-group.sql
@@ -3,6 +3,8 @@ SELECT COUNT(*), YEAR(`enrolment_date`) FROM `students` GROUP BY YEAR(`enrolment
 
 
 -- 2. Contare gli insegnanti che hanno l ufficio nello stesso edificio
+SELECT COUNT(*), `office_address` FROM `teachers` GROUP BY `office_address`;
+
 
 
 -- 3. Calcolare la media dei voti di ogni appello d esame
