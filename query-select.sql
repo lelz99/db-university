9a9f1c7154329694029f1b46d27e27fc34f6943e
lelz99 <emanuelesimonetti99@gmail.com>
2024-03-01 14:24:00 +0100

    es 3 select

--- query-select.sql
+++ query-select.sql
@@ -7,6 +7,7 @@ SELECT * FROM `courses` WHERE `cfu` > 10;
 
 
 3. Selezionare tutti gli studenti che hanno più di 30 anni
+SELECT * FROM `students` WHERE `date_of_birth` <= '1994-03-01' ORDER BY `date_of_birth` DESC;
 
 
 4. Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di
