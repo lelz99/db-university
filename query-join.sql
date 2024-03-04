c84035b09ea2793e9d4c10a96d92456d3dc67877
lelz99 <emanuelesimonetti99@gmail.com>
2024-03-04 15:43:28 +0100

    es 1 join

--- query-join.sql
+++ query-join.sql
@@ -1,5 +1,5 @@
 -- 1. Selezionare tutti gli studenti iscritti al Corso di Laurea in Economia
-
+SELECT S.`id`, S.`name`, S.`surname`, D.`name` FROM `students` AS S JOIN `degrees` AS D ON D.`id` = S.`degree_id` WHERE D.`name` = 'Corso di Laurea in Economia';
 
 -- 2. Selezionare tutti i Corsi di Laurea del Dipartimento di Neuroscienze
 
