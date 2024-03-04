3ecd8b4ddb01f5fe4835ab751f9ff9472d216b9b
lelz99 <emanuelesimonetti99@gmail.com>
2024-03-04 17:43:16 +0100

    es 4 join

--- query-join.sql
+++ query-join.sql
@@ -8,7 +8,7 @@ SELECT DEG.`id`, DEG.`name`, DEP.`name` FROM `degrees` AS DEG JOIN `departments`
 SELECT T.`id`, T.`name`, T.`surname`, C.`name` FROM `course_teacher` AS CT JOIN `teachers` AS T ON T.`id` = CT.`teacher_id` JOIN `courses` AS C ON C.`id` = CT.`course_id` WHERE T.`surname` = 'Amato' AND T.`name` = 'Fulvio';
 
 -- 4. Selezionare tutti gli studenti con i dati relativi al corso di laurea a cui sono iscritti e il relativo dipartimento, in ordine alfabetico per cognome e nome
-
+SELECT S.`id`, S.`surname`, S.`name`, DEG.`name`, DEP.`name` FROM `students` AS S JOIN `degrees` AS DEG ON S.`degree_id` = DEG.`id` JOIN `departments` AS DEP ON DEP.`id` = DEG.`department_id` ORDER BY S.`surname` DESC, S.`name` DESC;
 
 -- 5. Selezionare tutti i corsi di laurea con i relativi corsi e insegnanti
 SELECT D.`id`, D.`name`,C.`id`, C.`name`,T.`id`, T.`name`, T.`surname` FROM `degrees` AS D JOIN `courses` AS C ON D.`id` = C.`degree_id` JOIN `course_teacher` AS CT ON C.`id` = CT.`course_id` JOIN `teachers` AS T ON CT.`teacher_id` = T.`id`;
